194d8e019c51f245be7d20f48eb34ceae538b9ad
LucasAlfare <god-lucas@hotmail.com>
2023-04-13 15:32:22 -0300

    update main desktop window settings

--- src/main/kotlin/com/lucasalfare/florganizer/Main.kt
+++ src/main/kotlin/com/lucasalfare/florganizer/Main.kt
@@ -7,7 +7,12 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.runtime.snapshots.SnapshotStateList
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowPosition
+import androidx.compose.ui.window.WindowState
 import androidx.compose.ui.window.application
 import com.lucasalfare.fllistener.UIManager
 import com.lucasalfare.fllistener.setupManagers
@@ -83,7 +88,10 @@ fun App() {
 }
 
 fun main() = application {
-  Window(onCloseRequest = ::exitApplication) {
+  Window(
+    onCloseRequest = ::exitApplication,
+    state = WindowState(position = WindowPosition(Alignment.Center), size = DpSize(400.dp, 700.dp))
+  ) {
     LaunchedEffect(true) {
       setupManagers(
         PatientsManager(),
